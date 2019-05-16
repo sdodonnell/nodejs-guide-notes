28684baea9ee15227f8e867e10e0884c606b35b8
Sam <sam.d.odonnell@gmail.com>
2019-05-16 13:45:58 -0700

    Serving html pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 // Here we set up a sort of "mini Express app" that we can plug into our main one.
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"></form>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 })
 
 router.post('/product', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+// Path is a core module in Node that lets us use relative paths instead of absolute paths when we create and send files.
+const path = require('path');
+
+// Instead of sending raw HTML with res.send(), we can send an HTML file instead with res.sendFile().
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>')
+    // With path.join(), we pass in first __dirname, which is a globally available variable that finds the current folder in your file system (in this case, /routes), and then the specific folder and file name we want to write to.
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
 })
 
 module.exports = router;
