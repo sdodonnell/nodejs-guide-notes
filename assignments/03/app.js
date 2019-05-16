c8160b85bea9ee1745d218f358486051630b60a7
Sam <sam.d.odonnell@gmail.com>
2019-05-16 15:07:53 -0700

    Assignment 3

---
+++ assignments/03/app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/users', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', 'users.html'))
+})
+
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', 'main.html'))
+})
+
+const server = http.createServer(app);
+
+server.listen(3000);
