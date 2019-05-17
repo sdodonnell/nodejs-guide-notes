666e20d97a355fefb8a654e53d6aa1444234c499
Sam <sam.d.odonnell@gmail.com>
2019-05-17 15:53:53 -0700

    Sharing data across requests and users

--- routes/admin.js
+++ routes/admin.js
@@ -4,13 +4,19 @@ const path = require('path');
 // Here we set up a sort of "mini Express app" that we can plug into our main one.
 const router = express.Router();
 
+// We use a 'products' array to store the elements of our request when we make a POST request.
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'))
 })
 
 router.post('/product', (req, res, next) => {
-    console.log(req.body)
+    products.push({ title: req.body.title })
     res.redirect('/');
 })
 
-module.exports = router;
+module.exports = {
+    router,
+    products
+};
